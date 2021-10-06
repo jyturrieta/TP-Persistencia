e7c70664d7a14273f9f5e42a2a0d34cd77f26cc4
jyturrieta <joelyturrieta@gmail.com>
2021-10-05 23:19:26 -0300

    .

--- api/routes/materias.js
+++ api/routes/materias.js
@@ -2,19 +2,22 @@ var express = require("express");
 var router = express.Router();
 var models = require("../models");
 
-router.get("/", (req, res) => {
-  console.log("Esto es un mensaje para ver en consola");
-  models.materia
-    .findAll({
-      attributes: ["id", "nombre", "id_carrera"]
-    })
-    .then(materias => res.send(materias))
-    .catch(() => res.sendStatus(500));
+router.get("/", (req, res,next) => {
+
+  models.materia.findAll({attributes: ["id","nombre","id_carrera"],
+      
+      /////////se agrega la asociacion 
+      include:[{as:'Carrera-Relacionada', model:models.carrera, attributes: ["id","nombre"]}]
+      ////////////////////////////////
+
+    }).then(materias => res.send(materias)).catch(error => { return next(error)});
 });
+
+
 
 router.post("/", (req, res) => {
   models.materia
-    .create({ nombre: req.body.nombre, id_carrera: req.body.id_carrera })
+    .create({ nombre: req.body.nombre,id_carrera:req.body.id_carrera })
     .then(materia => res.status(201).send({ id: materia.id }))
     .catch(error => {
       if (error == "SequelizeUniqueConstraintError: Validation error") {
@@ -30,7 +33,7 @@ router.post("/", (req, res) => {
 const findmateria = (id, { onSuccess, onNotFound, onError }) => {
   models.materia
     .findOne({
-      attributes: ["id", "nombre", "id_carrera"],
+      attributes: ["id", "nombre"],
       where: { id }
     })
     .then(materia => (materia ? onSuccess(materia) : onNotFound()))
@@ -48,7 +51,7 @@ router.get("/:id", (req, res) => {
 router.put("/:id", (req, res) => {
   const onSuccess = materia =>
     materia
-      .update({ nombre: req.body.nombre, id_carrera: req.body.id_carrera }, { fields: ["nombre", "id_carrera"] })
+      .update({ nombre: req.body.nombre }, { fields: ["nombre"] })
       .then(() => res.sendStatus(200))
       .catch(error => {
         if (error == "SequelizeUniqueConstraintError: Validation error") {
